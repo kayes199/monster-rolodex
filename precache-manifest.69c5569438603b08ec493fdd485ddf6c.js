2dbbd5e6c2b3b7048d76b90cf2f6bb9c6b1d38e2
kayes uddin <kayesuddin739@gmail.com>
2020-09-23 19:54:40 +0600

    Updates

---
+++ precache-manifest.69c5569438603b08ec493fdd485ddf6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d78026e83ef908bd5f4e2c24884151e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a62775637b9e840a245a",
+    "url": "/monster-rolodex/static/css/main.d747a871.chunk.css"
+  },
+  {
+    "revision": "082f8914de693280636e",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9cbb02df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62775637b9e840a245a",
+    "url": "/monster-rolodex/static/js/main.fde12cd7.chunk.js"
+  },
+  {
+    "revision": "192894fcf2c1737177cd",
+    "url": "/monster-rolodex/static/js/runtime-main.df1e3023.js"
+  }
+]);
